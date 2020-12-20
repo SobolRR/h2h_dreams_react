61573e6b63f53e1512cd0ded62066322f3244efb
Roman Sobol <roman.sobol@dataart.com>
2020-12-20 10:21:44 +0200

    feat: fix links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
         <div class="container">
             <div class="footer__inner">
                 <div class="footer__item footer__item--left social-links-list">
-                    <a href="https://www.facebook.com/heart2heart.ua" class="social-links-list__item">
+                    <a href="https://www.facebook.com/heart2heart.ua" class="social-links-list__item" target="_blank">
                         <img src={facebook} alt="facebook link"/>
                     </a>
-                    <a href="https://www.instagram.com/h2h_charity/" class="social-links-list__item">
+                    <a href="https://www.instagram.com/h2h_charity/" class="social-links-list__item" target="_blank">
                         <img src={instagram} alt="instagram link"/>
                     </a>
                     {/* <a href="#" class="social-links-list__item">
@@ -19,8 +19,8 @@ function Footer() {
                     </a> */}
                 </div>
                 <div class="footer__item footer__contacts footer__item--center">
-                    <a href="tel:+380 96 78 40 245" class="item" target="_blank">+380 96 78 40 245</a>
-                    <a href="tel:+380 67 318 38 76" class="item" target="_blank">+380 67 318 38 76</a>
+                    <a href="tel:+380 96 78 40 245" class="item" >+380 96 78 40 245</a>
+                    <a href="tel:+380 67 318 38 76" class="item">+380 67 318 38 76</a>
                 </div>
                 <div class="footer__item footer__item--right text-right">
                     <Link to="/dreams" class="btn">Исполнить мечту</Link>
